76f5a369c288df13c4e2f6ef22e956cca0e4e662
Christopher Lewis <61091285+cmlcode@users.noreply.github.com>
2024-09-07 17:13:33 -0500

    Added add test

--- resources/tests/test_dbConn.js
+++ resources/tests/test_dbConn.js
@@ -1,10 +1,12 @@
-import { getAllRssFeedUrls, saveRssFeedUrl, deleteRssFeedUrl, getDb, saveRssItem } from "../scripts/dbConn.js";
+import { getAllRssFeedUrls, saveRssFeedUrl, deleteRssFeedUrl, getDb,
+  saveRssItem, getAllRssItems } from "../scripts/dbConn.js";
+
 
 var failedTests = 0;
 var runTests = 0;
 
 const TEST_URL_1 = "https://feeds.megaphone.fm/newheights";
-const TEST_TILE = "TEST_RSS_TILE";
+const TEST_TITLE = "TEST_RSS_TILE";
 const TEST_DESC = "TEST_RSS_DESC";
 const TEST_OLD_PUB_DATE = "Wed, 19 Jul 2023 09:50:00 -0000";
 
@@ -29,7 +31,7 @@ async function test_getAllRssFeedUrls_empty(){
   const rssUrls = await getAllRssFeedUrls(dbObj)
   if (rssUrls.length != 0){
     failedTests++;
-    console.log("FAIL: test_getAllRssFeedUrls_empty");
+    console.log("FAIL:test_getAllRssFeedUrls_empty");
   }
 }
 
@@ -40,13 +42,13 @@ async function test_saveRssFeedUrl() {
   let rssUrls = await getAllRssFeedUrls(dbObj);
   if (rssUrls.length != 1){
     failedTests++;
-    console.log("FAIL: test_saveRssFeedUrl:saveRssFeedUrl");
+    console.log("FAIL:test_saveRssFeedUrl:saveRssFeedUrl");
   }
   deleteRssFeedUrl(dbObj, TEST_URL_1);
   rssUrls = await getAllRssFeedUrls(dbObj);
   if (rssUrls.length != 0){
     failedTests++;
-    console.log("FAIL: test_saveRssFeedUrl:deleteRssFeedUrl");
+    console.log("FAIL:test_saveRssFeedUrl:deleteRssFeedUrl");
   }
 }
 
@@ -55,15 +57,41 @@ async function test_saveRssItem() {
   const dbObj = await getDb();
   saveRssItem(dbObj, TEST_TITLE, TEST_DESC, TEST_OLD_PUB_DATE, TEST_URL_1);
 
+  let rssUrls = await getAllRssFeedUrls(dbObj);
   if (rssUrls.length != 1){
     failedTests++;
-    console.log("FAIL: test_saveRssItem:Failed to save rssUrl");
+    console.log("FAIL:test_saveRssItem:Failed to save rssUrl");
+    return;
   }
+
+  const rssItems = await getAllRssItems(dbObj);
+  if (rssItems.length != 1){
+    failedTests++;
+    console.log("FAIL:test_saveRssItem:Failed to save rssObj");
+  }
+  const rssObj = rssItems[0];
+  if (rssObj['title'] != TEST_TITLE) {
+    failedTests++;
+    console.log("FAIL:test_saveRssItem:Failed to save rssObj title");
+  }
+  if (rssObj['desc'] != TEST_DESC) {
+    failedTests++;
+    console.log("FAIL:test_saveRssItem:Failed to save rssObj desc");
+  }
+  if (rssObj['pubDate'] != TEST_OLD_PUB_DATE){
+    failedTests++;
+    console.log("FAIL:test_saveRssItem:Failed to save rssObj pub date");
+  }
+  if (rssObj['rssFeed'] != TEST_URL_1){
+    failedTests++;
+    console.log("FAIL:test_saveRssItem:Failed to save rssObj url");
+  }
+
   deleteRssFeedUrl(dbObj, TEST_URL_1);
   rssUrls = await getAllRssFeedUrls(dbObj);
   if (rssUrls.length != 0){
     failedTests++;
-    console.log("FAIL: test_saveRssItem:deleteRssFeedUrl");
+    console.log("FAIL:test_saveRssItem:deleteRssFeedUrl");
   }
   
 
@@ -74,6 +102,8 @@ async function runTestSuite() {
   await set_test_env(databaseName)
   await test_getAllRssFeedUrls_empty();
   await test_saveRssFeedUrl();
+  await test_saveRssItem();
+  console.log("TESTS RUN: " + runTests);
   console.log("TESTS FAILD: " + failedTests);
   console.log("FAILURE RATE: " + failedTests/runTests*100 + "%");
 }
