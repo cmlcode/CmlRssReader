96f40611bf03703020e4a2cd5b3934e909d7684a
Chris Lewis <61091285+cmlcode@users.noreply.github.com>
2024-09-12 19:08:26 -0500

    Added testing UI (#3)
    Co-authored-by: Chris Lewis <Chris Lewis>

---
+++ resources/tests/test_rssFetcher.js
@@ -0,0 +1,5 @@
+export async function runTestSuite_rssFetcher() {
+  document.getElementById('testsRan_rssFetcher').innerText = `Tests Ran = 0`; 
+  document.getElementById('testsFailed_rssFetcher').innerText = `Tests Failed = 0`; 
+  document.getElementById('testFailedPercent_rssFetcher').innerText = `Tests Failed Percent = 0.00%`;
+}
---
+++ resources/tests/test_rssManager.js
@@ -0,0 +1,5 @@
+export async function runTestSuite_rssManager() {
+  document.getElementById('testsRan_rssManager').innerText = `Tests Ran = 0`; 
+  document.getElementById('testsFailed_rssManager').innerText = `Tests Failed = 0`; 
+  document.getElementById('testFailedPercent_rssManager').innerText = `Tests Failed Percent = 0.00%`;
+}
